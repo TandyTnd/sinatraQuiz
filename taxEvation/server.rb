b63784b0fdf0dc3372ba28bf8a9d60495813bcd9
Ubuntu <ubuntu@ip-172-31-33-141.ec2.internal>
2021-06-04 01:44:41 +0000

    ya casi

--- taxEvation/server.rb
+++ taxEvation/server.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'faraday'
 require 'sinatra'
+require 'time'
 
 URL = 'https://kss1ggcwh4.execute-api.us-west-2.amazonaws.com/default/books'
 BASE = 'https://3r690kl9q2.execute-api.us-east-1.amazonaws.com/default/Question'
@@ -9,7 +10,7 @@ lambdaCheckAnswer = "https://t4upfkbyug.execute-api.us-east-1.amazonaws.com/defa
 lambdaHighscore = "https://8reoi7vn9l.execute-api.us-east-1.amazonaws.com/default/HighScore"
 
 
-POSIBLE = [0,1,2]
+enable :sessions
 
 get '/' do
   erb :index
@@ -23,12 +24,19 @@ get '/ejemplo' do
 end
 
 get '/quiz/:id' do
-  parameters = '?id=0'
+  @idPregunta =  params[:id]
+  
+  parameters = '?id=' + @idPregunta
   questionURL = BASE + parameters
   connection = Faraday.new(url: questionURL)
   response = connection.get
+  session[:idPregunta] = @idPregunta
+
+  
   @question = ''
   @answers = []
+  @preguntaActual = session[:preguntaActual]
+  @cantidadPreguntas = session[:cantidadPreguntas]
   if response.success?
     data = JSON.parse(response.body)
     puts data
@@ -38,12 +46,25 @@ get '/quiz/:id' do
   erb :quiz
 end
 
+get '/checkResults' do
+    @usuario = session[:usuario]
+    @cantidadPreguntas = session[:cantidadPreguntas]
+    @score = session[:score]
+    @horaInicio = session[:horaInicio]
+    @horaFinal = Time.now.getutc
+  erb :checkResults
+end
+
+
 post'/submitAnswer' do
+  puts "sumbit answer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
   @answer = params[:option]
-  redirect '/checkAnswer/0/'+ @answer
+  @idPregunta = session[:idPregunta]
+    redirect '/checkAnswer/' + @idPregunta.to_s + '/' + @answer.to_s
 end
 
 get'/checkAnswer/:id/:answer' do
+  puts "checkAnswer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
   parameters = '?id=' + params[:id] + "&answer=" + params[:answer] 
   questionURL = lambdaCheckAnswer + parameters
   connection = Faraday.new(url: questionURL)
@@ -51,6 +72,9 @@ get'/checkAnswer/:id/:answer' do
   @question = ""
   @rightData = ""
   @correct = ""
+  @preguntaActual = session[:preguntaActual]
+  @cantidadPreguntas = session[:cantidadPreguntas]
+
   if response.success?
     data = JSON.parse(response.body)
     puts data
@@ -58,7 +82,23 @@ get'/checkAnswer/:id/:answer' do
     @rightData = data['right']
     @correct = data['correct']
   end
+  
+  if @correct
+    session[:score] = session[:score] + 1
+  end
     erb :checkAnswer 
+end
+
+post '/siguientePregunta' do
+
+    if !session[:listaPreguntas].empty? 
+      @siguientePregunta = session[:listaPreguntas].pop
+      session[:preguntaActual] = session[:preguntaActual] + 1
+      redirect '/quiz/' + @siguientePregunta.to_s
+    else
+      redirect '/checkResults'
+    end  
+
 end
 
 get '/resultado/:score' do
@@ -71,9 +111,21 @@ end
 
 
 post'/iniciaQuiz' do
-  @answer = params[:customRange1]
-  "Hello #{@answer}"
-  #redirect '/checkAnswer/0/'+ @answer
+  cantidadPreguntas = params[:customRange1]
+  cantidadPreguntas =Integer(cantidadPreguntas)
+  usuario = params[:idUsuario]
+  session[:usuario] = usuario
+  session[:cantidadPreguntas] = cantidadPreguntas
+  session[:preguntaActual] = 1
+  session[:score] = 0
+  b = (0..2).to_a
+  session[:listaPreguntas] = b.sample(cantidadPreguntas)
+  primerPregunta = session[:listaPreguntas].pop
+  
+  
+  session[:horaInicio] = Time.now.getutc
+  puts session[:horaInicio]
+  redirect '/quiz/' + primerPregunta.to_s
 end
 
 
@@ -87,4 +139,8 @@ get'/highscore' do
     end
     puts @highscores
     erb :highscore
+end
+
+post '/postHighscore' do
+  
 end
